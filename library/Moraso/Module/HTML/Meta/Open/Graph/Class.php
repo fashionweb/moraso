ac89dd6cc6b1a493831f92f27a8694faa389b1d8
Christian Kehres <c.kehres@webtischlerei.de>
2013-07-13 11:59:31 +0200

    minor improvement

--- library/Moraso/Module/HTML/Meta/Open/Graph/Class.php
+++ library/Moraso/Module/HTML/Meta/Open/Graph/Class.php
@@ -10,25 +10,32 @@ class Moraso_Module_HTML_Meta_Open_Graph_Class extends Moraso_Module_Abstract
 
     protected function _main()
     {
-        $idart = Aitsu_Registry::get()->env->idartlang;
+        $idart = Aitsu_Registry::get()->env->idart;
+        $idcat = Aitsu_Registry::get()->env->idcat;
         $idartlang = Aitsu_Registry::get()->env->idartlang;
-        
+
         // get Data
-        $article = Aitsu_Core_Article::factory();
+        $article = Aitsu_Persistence_Article::factory($idart);
         $articleProperty = Aitsu_Persistence_ArticleProperty::factory($idartlang)->load();
-        
+
         $open_graph = (object) $articleProperty->open_graph;
+
+        if ($article->isIndex()) {
+            $url = Moraso_Rewrite_Standard::getInstance()->rewriteOutput('{ref:idcat-' . $idcat . '}');
+        } else {
+            $url = Moraso_Rewrite_Standard::getInstance()->rewriteOutput('{ref:idart-' . $idart . '}');
+        }
 
         // set Graph Data
         $data = array(
             'og:title' => $article->pagetitle . $this->_params->title_suffix,
             'og:type' => isset($open_graph->type->value) && !empty($open_graph->type->value) ? $open_graph->type->value : 'website',
             'og:image' => Moraso_Html_Helper_Image::getPath($idart, $article->mainimage, 1500, 1500, 2),
-            'og:url' => Moraso_Rewrite_Standard::getInstance()->rewriteOutput('{ref:idart-' . $idart . '}'),
+            'og:url' => $url,
             'og:site_name' => $this->_params->site_name,
             'og:locale' => 'de_DE'
         );
-        
+
         // create View
         $view = $this->_getView();
         $view->data = $data;

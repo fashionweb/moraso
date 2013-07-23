fb9e97535ebc24ab59f5d1f71418f6faad903eb8
Christian Kehres <c.kehres@webtischlerei.de>
2013-07-24 00:51:58 +0200

    Addresses Plugin eingebunden

---
+++ library/Moraso/Plugin/Addresses/Generic/Class.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * @author Christian Kehres <c.kehres@webtischlerei.de>
+ * @copyright (c) 2013, webtischlerei <http://www.webtischlerei.de>
+ */
+class Moraso_Plugin_Addresses_Generic_Controller extends Moraso_Adm_Plugin_Controller
+{
+    public function init()
+    {
+        $this->_helper->layout->disableLayout();
+    }
+
+    public function indexAction()
+    {
+        header("Content-type: text/javascript");
+    }
+
+    public function storeAction()
+    {
+        $addresses = Moraso_Addresses::get();
+
+        $total = count((array) $addresses->userInput);
+
+        $this->_helper->json((object) array(
+                    'message' => $total . ' Adressen wurden geladen',
+                    'addresses' => (array) $addresses->userInput,
+                    'success' => true,
+                    'total' => $total
+        ));
+    }
+
+    public function editAction()
+    {
+        $address_id = $this->getRequest()->getParam('address_id');
+
+        $this->_helper->layout->disableLayout();
+
+        $classExplode = explode('_', __CLASS__);
+
+        $form = Aitsu_Forms::factory(strtolower($classExplode[2]), APPLICATION_LIBPATH . '/' . $classExplode[0] . '/' . $classExplode[1] . '/' . $classExplode[2] . '/' . $classExplode[3] . '/forms/address.ini');
+        $form->title = Aitsu_Translate::translate('Edit Address');
+        $form->url = $this->view->url(array('paction' => 'edit'));
+
+        $addresses_groups = Moraso_Addresses::getGroups();
+
+        $groups = array();
+        foreach ($addresses_groups as $addresses_group) {
+            $groups[] = (object) array(
+                        'value' => $addresses_group['addresses_group_id'],
+                        'name' => $addresses_group['name']
+            );
+        }
+
+        $form->setOptions('groups', $groups);
+
+        $address = Moraso_Addresses::get($address_id);
+
+        if ($address) {
+            $form->setValues((array) $address->googleOutput);
+            $form->setValues(array('groups' => (array) $address->groups));
+            $form->setValues((array) $address->userInput);
+        }
+
+        if ($this->getRequest()->getParam('loader')) {
+            $this->view->form = $form;
+            header("Content-type: text/javascript");
+            return;
+        }
+
+        try {
+            if ($form->isValid()) {
+                $address = $form->getValues();
+
+                Moraso_Addresses::set($address['name'], $address['street'], $address['house_number'], $address['postal_code'], $address['city'], $address['address_id'], $address['groups']);
+
+                $this->_helper->json((object) array(
+                            'success' => true,
+                            'address' => (object) $address
+                ));
+            } else {
+                $this->_helper->json((object) array(
+                            'success' => false,
+                            'errors' => $form->getErrors()
+                ));
+            }
+        } catch (Exception $e) {
+            $this->_helper->json((object) array(
+                        'success' => false,
+                        'exception' => true,
+                        'message' => $e->getMessage()
+            ));
+        }
+    }
+
+    public function deleteAction()
+    {
+        $this->_helper->layout->disableLayout();
+
+        Moraso_Addresses::delete($this->getRequest()->getParam('address_id'));
+
+        $this->_helper->json((object) array(
+                    'success' => true
+        ));
+    }
+
+}

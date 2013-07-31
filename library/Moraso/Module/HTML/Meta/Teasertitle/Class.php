9d7e408b026ee9d61cc6e8fa71f89d292d69390f
Christian Kehres <c.kehres@webtischlerei.de>
2013-07-31 10:18:10 +0200

    "HTML.Meta.Teasertitle" und "Date.Created" hinzugefügt

---
+++ library/Moraso/Module/HTML/Meta/Teasertitle/Class.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @author Christian Kehres <c.kehres@webtischlerei.de>
+ * @copyright (c) 2013, webtischlerei <http://www.webtischlerei.de>
+ */
+class Moraso_Module_HTML_Meta_Teasertitle_Class extends Moraso_Module_Abstract
+{
+    protected function _main()
+    {
+        $teasertitle = Aitsu_Content_Text::get('Teasertitle', 0);
+
+        if (empty($teasertitle)) {
+            $teasertitle = stripslashes(Aitsu_Core_Article::factory()->teaertitle);
+        }
+
+        $view = $this->_getView();
+
+        $view->tag = $this->_params->tag;
+        $view->teasertitle = htmlentities($teasertitle, ENT_COMPAT, 'UTF-8');
+
+        return $view->render('index.phtml');
+    }
+
+    protected function _cachingPeriod()
+    {
+        return 'eternal';
+    }
+
+}

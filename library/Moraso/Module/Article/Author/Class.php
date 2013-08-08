a6d9317cbb3ee2766acf3746bb0e37b74ab825c1
Christian Kehres <c.kehres@webtischlerei.de>
2013-08-08 11:19:51 +0200

    Update: 1.28.2-92 to 1.28.2-93

--- library/Moraso/Module/Article/Author/Class.php
+++ library/Moraso/Module/Article/Author/Class.php
@@ -10,6 +10,6 @@ class Moraso_Module_Article_Author_Class extends Moraso_Module_Abstract
 
     protected function _main()
     {
-        $this->_view->author = Moraso_Db::simpleFetch('author', '_art_meta', array('idartlang' => $this->_defaults['idartlang']), 1, 'eternal');s
+        $this->_view->author = Moraso_Db::simpleFetch('author', '_art_meta', array('idartlang' => $this->_defaults['idartlang']), 1, 'eternal');
     }
 }

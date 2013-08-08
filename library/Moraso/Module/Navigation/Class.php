7f73b6f4f8de22e9360d43b94ab0850a2407a1f7
Christian Kehres <c.kehres@webtischlerei.de>
2013-08-08 21:48:13 +0200

    Update: 1.28.4-98 to 1.28.5-99

--- library/Moraso/Module/Navigation/Class.php
+++ library/Moraso/Module/Navigation/Class.php
@@ -4,31 +4,19 @@
  * @author Christian Kehres <c.kehres@webtischlerei.de>
  * @copyright (c) 2013, webtischlerei <http://www.webtischlerei.de>
  */
-class Moraso_Module_Navigation_Class extends Moraso_Module_Abstract {
-
+class Moraso_Module_Navigation_Class extends Moraso_Module_Abstract
+{
+    protected $_newRenderingMethode = true;
     protected $type = 'navigation';
-    protected $_allowEdit = false;
 
-    protected function _main() {
-
-        $view = $this->_getView();
-        
-        $template = isset($this->_params->template) ? $this->_params->template : 'index';
-        
-        if (isset($this->_params->idcat) && !empty($this->_params->idcat)) {
-            $idcat = $this->_params->idcat;
+    protected function _main()
+    {                
+        if (isset($this->_defaults->idcat) && !empty($this->_defaults->idcat)) {
+            $idcat = $this->_defaults->idcat;
         } else {
             $idcat = Moraso_Config::get('navigation.' . $this->_index);
         }
         
-        $view->nav = Moraso_Navigation_Frontend::getTree($idcat);
-
-        return $view->render($template . '.phtml');
+        $this->_view->nav = Moraso_Navigation_Frontend::getTree($idcat);
     }
-
-    protected function _cachingPeriod() {
-
-        return 'eternal';
-    }
-
 }
--- library/Moraso/Status.php
+++ library/Moraso/Status.php
@@ -8,7 +8,7 @@ class Moraso_Status
 {
     public static function version()
     {
-        $version = '$/major/1/minor/28/revision/4/build/98$';
+        $version = '$/major/1/minor/28/revision/5/build/99$';
 
         return str_replace(array(
             '/major/',

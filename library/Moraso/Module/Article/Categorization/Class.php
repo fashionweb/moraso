70490df0046c8a71da5871236cdb9bf30a5afc01
Christian Kehres <c.kehres@webtischlerei.de>
2013-08-08 14:06:49 +0200

    Das Module "Article.Categorization" auf die neue ModulRendering Methode umgebaut.

--- library/Moraso/Module/Article/Categorization/Class.php
+++ library/Moraso/Module/Article/Categorization/Class.php
@@ -6,40 +6,10 @@
  */
 class Moraso_Module_Article_Categorization_Class extends Moraso_Module_Abstract
 {
-    protected function _getDefaults()
-    {
-        $defaults = array(
-            'idart' => Aitsu_Registry::get()->env->idart,
-            'template' => 'index',
-            'configurable' => array(
-                'template' => true
-            )
-        );
-
-        return $defaults;
-    }
+    protected $_newRenderingMethode = true;
 
     protected function _main()
     {
-        $defaults = $this->_moduleConfigDefaults;
-
-        $translation = array();
-        $translation['configuration'] = Aitsu_Translate::_('Configuration');
-
-        if ($defaults['configurable']['template']) {
-            $template = Aitsu_Content_Config_Select::set($this->_index, 'template', Aitsu_Translate::_('Template'), $this->_getTemplates(), $translation['configuration']);
-        }
-
-        $template = !empty($template) ? $template : $defaults['template'];
-
-        $view = $this->_getView();
-        $view->categories = Moraso_Categorize::get($defaults['idart']);
-        return $view->render($template . '.phtml');
+        $view->categories = Moraso_Categorize::get($this->_defaults['idart']);
     }
-
-    protected function _cachingPeriod()
-    {
-        return 'eternal';
-    }
-
 }

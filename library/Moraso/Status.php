9c50c077bec271973f5b835ab9b421d227746093
Christian Kehres <c.kehres@webtischlerei.de>
2013-08-09 22:09:02 +0200

    Update: 1.33.1-127 to 1.33.1-128

--- library/Moraso/Status.php
+++ library/Moraso/Status.php
@@ -8,7 +8,7 @@ class Moraso_Status
 {
     public static function version()
     {
-        $version = '$/major/1/minor/33/revision/1/build/127$';
+        $version = '$/major/1/minor/33/revision/1/build/128$';
 
         return str_replace(array(
             '/major/',

a4ba14aa5984c92c4c68d8d2fafc30b503a7070f
Christian Kehres <c.kehres@webtischlerei.de>
2013-08-10 11:33:49 +0200

    Update: 1.33.3-133 to 1.33.3-134

--- library/Moraso/Status.php
+++ library/Moraso/Status.php
@@ -8,7 +8,7 @@ class Moraso_Status
 {
     public static function version()
     {
-        $version = '$/major/1/minor/33/revision/3/build/133$';
+        $version = '$/major/1/minor/33/revision/3/build/134$';
 
         return str_replace(array(
             '/major/',
@@ -16,13 +16,22 @@ class Moraso_Status
             '/revision/',
             '/build/',
             '$'
-                ), array(
+            ), array(
             '',
             '.',
             '.',
             '-',
             ''
-                ), $version);
+            ), $version);
     }
 
+    public static function latestVersion()
+    {
+        $applicationStatus = file_get_contents('https://bitbucket.org/webtischlerei/moraso-cms/raw/5cfde741b36d61962bd2848f71be460d1afedbb5/library/Moraso/Status.php');
+
+        $matches = array();
+        preg_match_all('@\'\\$/major/(\\d+)/minor/(\\d+)/revision/(\\d+)/build/(\\d+)\\$\'@', $applicationStatus, $matches);
+
+        return $matches[1][0] . '.' . $matches[2][0] . '.' . $matches[3][0] . '-' . $matches[4][0];
+    }
 }
